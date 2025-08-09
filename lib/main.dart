5bc0cc95a89d9651bce4ba8864f42889bf0e58ea
kasem123456789 <6749010027@phuketvc.ac.th>
2025-08-09 15:05:51 +0700

    f3

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  //runApp(const MyApp());
-
-  //const app = MaterialApp(title:"oll",home:Text("Helloo World!"));
-  //runApp(app);
-
-  runApp(MaterialApp(title:"ห้วข้อ",home:Scaffold(appBar:AppBar(title: Text("My App"),backgroundColor: Colors.pinkAccent,centerTitle: true,),
-  body: Text("Hello Flutter!"),
-  ),));
+  runApp(MaterialApp(
+    title: "หัวข้อ",
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("My App"),
+        backgroundColor: Colors.pinkAccent,
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text("Hello Flutter!"),
+      ),
+    ),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -19,59 +24,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
+class Home extends StatelessWidget {
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+    return const Center(
+      child: Text("Hello Flutter! Step2"),
     );
   }
 }
