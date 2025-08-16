7020bcdb785d2858aac0bdfa1031ad4621a1bd2d
kasem123456789 <6749010027@phuketvc.ac.th>
2025-08-16 10:34:43 +0700

    step : 5

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,25 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // return Text("Hello Flutter Step 2");
+    return Center(
+      child: Container(
+        color: Colors.deepPurpleAccent,
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        padding: EdgeInsets.all(50),
+        child: Text(
+          "Hello Flutter Step4",
+          style: TextStyle(
+            fontSize: 25,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
